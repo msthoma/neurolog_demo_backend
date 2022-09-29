4e34838391e693c3db1aa62eb5114eeac631280f
marios <msthoma@yahoo.com>
2022-09-29 13:15:49 +0300

    New improved neural net

--- models/model.py
+++ models/model.py
@@ -2,28 +2,22 @@ import torch
 import torch.nn as nn
 
 
-class Sum2NN(nn.Module):
-    def __init__(self, N=10):
-        super(Sum2NN, self).__init__()
+class MnistNNForNeurologDemo(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 6, 5),
+            nn.Conv2d(1, 32, 3, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, 3, 1),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),
-            nn.ReLU(True),
-            nn.Conv2d(6, 16, 5),
-            nn.MaxPool2d(2, 2),
-            nn.ReLU(True)
-        )
-        self.classifier = nn.Sequential(
-            nn.Linear(16 * 4 * 4, 120),
-            nn.ReLU(),
-            nn.Linear(120, 84),
-            nn.ReLU(),
-            nn.Linear(84, N),
-            nn.Softmax(1)
+            nn.Dropout2d(0.25),
+            nn.Flatten(start_dim=1),
+            nn.Linear(9216, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(128, 10),
         )
 
     def forward(self, vector: torch.Tensor):
-        x = self.encoder(vector)
-        x = x.view(-1, 16 * 4 * 4)
-        x = self.classifier(x)
-        return x
+        return self.encoder(vector)
